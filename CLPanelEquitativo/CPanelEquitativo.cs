99438742d650580b1928c292aca481b53ef5f408
David Góngora <fdgongora@iesmurgi.org>
2020-01-09 12:48:05 +0100

    sdsd

---
+++ CLPanelEquitativo/CPanelEquitativo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CLPanelEquitativo
+{
+    public class CPanelEquitativo: Panel
+    {
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (UIElement child in Children)
+            {
+                child.Measure(availableSize);
+            }
+            return availableSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            foreach (UIElement child in Children)
+            {
+                child.Arrange(new Rect(new Point(100, 100), child.DesiredSize));
+            }
+            return finalSize;
+        }
+    }
+
+}
